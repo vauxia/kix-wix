39b07525450719dd32fc962a06ca3a69ed0120c6
Allie Micka <allie@bluebird.io>
2025-08-09 14:55:22 -0500

    Add some rewrite rules

--- index.js
+++ index.js
@@ -2,27 +2,67 @@ export default {
   async fetch(request, env, ctx) {
     const url = new URL(request.url)
     
+    // Configuration - you can modify these
+    const TARGET_ORIGIN = env.TARGET_ORIGIN || 'https://allie2490.wixsite.com/welcome-cheetos'
+    const YOUR_DOMAIN = url.hostname // Automatically use the current domain
+    
+    // Parse the target URL
+    const targetURL = new URL(TARGET_ORIGIN)
+    const targetHost = targetURL.hostname
+    const targetPath = targetURL.pathname
+    
+    // Only proxy requests to your domain
+    if (url.hostname === YOUR_DOMAIN) {
       // Construct the target URL
-      const targetUrl = `https://allie2490.wixsite.com/welcome-cheetos${url.pathname}${url.search}`
+      const targetUrl = `${targetURL.origin}${targetPath}${url.pathname}${url.search}`
       
-      // Create new request with modified URL but same headers/method
+      // Create new request
       const modifiedRequest = new Request(targetUrl, {
         method: request.method,
-        headers: request.headers,
-        body: request.body
+        headers: {
+          ...request.headers,
+          'Host': targetHost,
+          'Origin': targetURL.origin,
+          'Referer': TARGET_ORIGIN
+        },
+        body: request.method !== 'GET' && request.method !== 'HEAD' ? request.body : undefined
       })
       
-      // Fetch from the target and return response
-      const response = await fetch(modifiedRequest)
-      
-      // Create new response with modified headers if needed
-      const newResponse = new Response(response.body, response)
-      
-      // Remove headers that might cause issues
-      newResponse.headers.delete('x-frame-options')
-      newResponse.headers.delete('content-security-policy')
-      
-      return newResponse
+      try {
+        const response = await fetch(modifiedRequest)
+        
+        // Get the response body as text to modify it
+        let body = await response.text()
+        
+        // Replace target domain references with your domain in the HTML/CSS/JS
+        body = body.replace(new RegExp(TARGET_ORIGIN.replace(/[.*+?^${}()|[\]\\]/g, '\\$&'), 'g'), `https://${YOUR_DOMAIN}`)
+        body = body.replace(new RegExp(targetHost + targetPath, 'g'), YOUR_DOMAIN)
+        body = body.replace(new RegExp(targetPath + '/', 'g'), '/')
+        
+        // Create new response with modified body
+        const newResponse = new Response(body, {
+          status: response.status,
+          statusText: response.statusText,
+          headers: {
+            ...response.headers,
+            'Access-Control-Allow-Origin': '*',
+            'Access-Control-Allow-Methods': 'GET, POST, PUT, DELETE, OPTIONS',
+            'Access-Control-Allow-Headers': 'Content-Type, Authorization'
+          }
+        })
+        
+        // Remove problematic headers
+        newResponse.headers.delete('x-frame-options')
+        newResponse.headers.delete('content-security-policy')
+        
+        return newResponse
+        
+      } catch (error) {
+        return new Response('Proxy Error: ' + error.message, { status: 500 })
+      }
+    }
     
+    // For other hostnames, return original request
+    return fetch(request)
   }
 }
