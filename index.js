bd2c39636decf925b45a69ec7af71ec1a45bb640
Allie Micka <allie@bluebird.io>
2025-08-09 14:19:45 -0500

    Initial commit

---
+++ index.js
@@ -0,0 +1,35 @@
+addEventListener('fetch', event => {
+    event.respondWith(handleRequest(event.request))
+  })
+  
+  async function handleRequest(request) {
+    const url = new URL(request.url)
+    
+    // Only proxy requests to www.xyz.com
+    if (url.hostname === 'cheet.tropicalsea.com') {
+      // Construct the target URL
+      const targetUrl = `https://allie2490.wixsite.com/welcome-cheetos${url.pathname}${url.search}`
+      
+      // Create new request with modified URL but same headers/method
+      const modifiedRequest = new Request(targetUrl, {
+        method: request.method,
+        headers: request.headers,
+        body: request.body
+      })
+      
+      // Fetch from the target and return response
+      const response = await fetch(modifiedRequest)
+      
+      // Create new response with modified headers if needed
+      const newResponse = new Response(response.body, response)
+      
+      // Remove headers that might cause issues
+      newResponse.headers.delete('x-frame-options')
+      newResponse.headers.delete('content-security-policy')
+      
+      return newResponse
+    }
+    
+    // For other hostnames, return original request
+    return fetch(request)
+  }
